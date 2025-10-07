f4ff2a03acad9ee7e8409bd12dd8410af4022f91
Jean Dannemann Carone <jean.carone@dev.pro>
2025-10-07 12:59:11 -0300

    SKELETON DONE. Final tweaks.

--- src/main/java/com/example/demo/controller/BananaController.java
+++ src/main/java/com/example/demo/controller/BananaController.java
@@ -4,7 +4,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-
 import java.time.Instant;
 import java.util.Map;
 import java.util.UUID;
